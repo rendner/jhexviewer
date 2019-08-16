f7c15e6a67fcaf646eb18ee57495eb3f22d78951
Daniel Schmidt <rendner@users.noreply.github.com>
2019-08-16 19:44:05 +0200

    'RowDimension' must have at least a width and height of 1

--- src/main/java/cms/rendner/hexviewer/core/model/row/template/RowDimension.java
+++ src/main/java/cms/rendner/hexviewer/core/model/row/template/RowDimension.java
@@ -1,5 +1,7 @@
 package cms.rendner.hexviewer.core.model.row.template;
 
+import cms.rendner.hexviewer.utils.CheckUtils;
+
 /**
  * The dimension of a row.
  *
@@ -19,12 +21,16 @@ public class RowDimension implements IRowTemplate.IDimension
     /**
      * Creates a new instance.
      *
-     * @param width  the width.
-     * @param height the height.
+     * @param width  the width, &gt;= 1.
+     * @param height the height, &gt;= 1.
      */
     public RowDimension(final int width, final int height)
     {
         super();
+
+        CheckUtils.checkMinValue(width, 1);
+        CheckUtils.checkMinValue(height, 1);
+
         this.width = width;
         this.height = height;
     }
