04a8a292db5d4498931ab221f0a6e05869658ee3
Daniel Schmidt <rendner@users.noreply.github.com>
2019-11-23 14:52:29 +0100

    provide example which highlights ASCII chars in different colors

--- src/main/java/cms/rendner/hexviewer/common/utils/AsciiUtils.java
+++ src/main/java/cms/rendner/hexviewer/common/utils/AsciiUtils.java
@@ -5,14 +5,26 @@ package cms.rendner.hexviewer.common.utils;
  *
  * @author rendner
  */
-public class AsciiUtils
+public final class AsciiUtils
 {
-    public static final char SPACE = 0x20;
-    public static final char HORIZONTAL_TAB = 0x09;
-    public static final char NEWLINE = 0x0A;
-    public static final char VERTICAL_TAB = 0x0B;
-    public static final char FEED = 0x0C;
-    public static final char CARRIAGE_RETURN = 0x0D;
+    public static final int NULL = 0x00;
+    public static final int HORIZONTAL_TAB = 0x09;
+    public static final int NEWLINE = 0x0A;
+    public static final int VERTICAL_TAB = 0x0B;
+    public static final int FEED = 0x0C;
+    public static final int CARRIAGE_RETURN = 0x0D;
+    public static final int SPACE = 0x20;
+
+    /**
+     * Checks whether c is any ASCII character.
+     *
+     * @param c character to be checked.
+     * @return <code>true</code> if indeed c is an ASCII character, <code>false</code> otherwise.
+     */
+    public static boolean isAscii(final int c)
+    {
+        return c >= 0 && c < 0x7F;
+    }
 
     /**
      * Checks whether c is a printable character.
@@ -23,7 +35,7 @@ public class AsciiUtils
      * @param c character to be checked.
      * @return <code>true</code> if indeed c is a printable character, <code>false</code> otherwise.
      */
-    public static boolean isPrintable(final char c)
+    public static boolean isPrintable(final int c)
     {
         return c >= 0x20 && c < 0x7F;
     }
@@ -34,7 +46,7 @@ public class AsciiUtils
      * @param c character to be checked.
      * @return <code>true</code> if indeed c is a white-space character, <code>false</code> otherwise.
      */
-    public static boolean isWhitespace(final char c)
+    public static boolean isWhitespace(final int c)
     {
         return c == SPACE ||
                 c == HORIZONTAL_TAB ||
@@ -42,6 +54,17 @@ public class AsciiUtils
                 c == VERTICAL_TAB ||
                 c == FEED ||
                 c == CARRIAGE_RETURN;
+    }
+
+    /**
+     * Checks whether c is a character with graphical representation.
+     *
+     * @param c character to be checked.
+     * @return <code>true</code> if indeed c has a graphical representation as character, <code>false</code> otherwise.
+     */
+    public static boolean isGraphic(final int c)
+    {
+        return c >= 0x21 && c < 0x7F;
     }
 
     /**
--- src/main/java/example/themes/console/ByteAreaColorProvider.java
+++ src/main/java/example/themes/console/ByteAreaColorProvider.java
@@ -1,5 +1,6 @@
 package example.themes.console;
 
+import cms.rendner.hexviewer.common.utils.AsciiUtils;
 import cms.rendner.hexviewer.view.JHexViewer;
 import cms.rendner.hexviewer.view.components.areas.bytes.ByteArea;
 import cms.rendner.hexviewer.view.components.areas.bytes.model.colors.IByteColorProvider;
@@ -8,22 +9,40 @@ import org.jetbrains.annotations.NotNull;
 import java.awt.*;
 
 /**
- * Example implementation of an {@link IByteColorProvider}.
+ * Example implementation of an {@link IByteColorProvider} which highlights ASCII chars in different colors.
  *
  * @author rendner
  */
 public final class ByteAreaColorProvider implements IByteColorProvider
 {
-    private @NotNull final ByteArea area;
-    private @NotNull final JHexViewer hexViewer;
+    private @NotNull
+    final ByteArea area;
+    private @NotNull
+    final JHexViewer hexViewer;
 
-    private @NotNull final Color focusedCaret = Color.white;
-    private @NotNull final Color caret = new Color(0xABB6B9);
-    private @NotNull final Color focusedSelection = new Color(0x616B65);
-    private @NotNull final Color selection = new Color(0x61, 0x6B, 0x65, 0xA5);
-    private @NotNull final Color rowElementForeground = Color.white;
-    private @NotNull final Color background = new Color(0x232629);
-    private @NotNull final Color highlight = new Color(0x6095B6);
+    private @NotNull
+    final Color focusedCaret = Color.WHITE;
+    private @NotNull
+    final Color caret = new Color(0xABB6B9);
+    private @NotNull
+    final Color focusedSelection = new Color(0x616B65);
+    private @NotNull
+    final Color selection = new Color(0x61, 0x6B, 0x65, 0xA5);
+    private @NotNull
+    final Color background = new Color(0x232629);
+    private @NotNull
+    final Color highlight = new Color(0x6095B6);
+
+    private @NotNull
+    final Color rowElementForegroundNull = Color.GRAY;
+    private @NotNull
+    final Color rowElementForegroundGraphic = Color.WHITE;
+    private @NotNull
+    final Color rowElementForegroundWhitespace = new Color(0x00CC00);
+    private @NotNull
+    final Color rowElementForegroundOtherAscii = new Color(0xFF9B9B);
+    private @NotNull
+    final Color rowElementForegroundNonAscii = new Color(0xFFCC33);
 
     public ByteAreaColorProvider(@NotNull final JHexViewer hexViewer, @NotNull final ByteArea area)
     {
@@ -56,7 +75,23 @@ public final class ByteAreaColorProvider implements IByteColorProvider
     @Override
     public Color getRowElementForeground(final int byteValue, final long offset, final int rowIndex, final int elementInRowIndex)
     {
-        return rowElementForeground;
+        if (AsciiUtils.NULL == byteValue)
+        {
+            return rowElementForegroundNull;
+        }
+        if (AsciiUtils.isGraphic(byteValue))
+        {
+            return rowElementForegroundGraphic;
+        }
+        if (AsciiUtils.isWhitespace(byteValue))
+        {
+            return rowElementForegroundWhitespace;
+        }
+        if (AsciiUtils.isAscii(byteValue))
+        {
+            return rowElementForegroundOtherAscii;
+        }
+        return rowElementForegroundNonAscii;
     }
 
     @NotNull
